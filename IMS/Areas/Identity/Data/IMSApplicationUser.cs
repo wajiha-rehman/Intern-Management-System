44ea98c3ba80b670602618c94ae330d883a846e3
wajiha.rehman <wajiha.rehman@SD-001>
2022-07-22 13:06:40 +0500

    first commit

---
+++ IMS/Areas/Identity/Data/IMSApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IMS.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the IMSApplicationUser class
+    public class IMSApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="nvarchar(100)")]
+        public string firstName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string lastName { get; set; }
+
+    }
+}
