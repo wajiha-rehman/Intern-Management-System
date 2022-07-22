f8e27ec1521597e6eb9f185cf4bd284619135197
wajiha.rehman <wajiha.rehman@SD-001>
2022-07-22 13:52:44 +0500

    Removing the row/col bootstrap in Login.cshtml and Register.cshtml

--- IMS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ IMS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,17 @@ namespace IMS.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
